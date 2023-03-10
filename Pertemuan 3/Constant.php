6ed8b93a4d3da70a7e3ae926d40ccbda57326e89
ezbim <bhima.pilot@gmail.com>
2023-03-10 10:50:15 +0700

    Pertemuan 3

---
+++ Pertemuan 3/Constant.php
@@ -0,0 +1,14 @@
+<?php
+
+    define("NAMA","Elza");
+    define("CLUB","Liverpool");
+
+    echo "Nama : ";
+    echo NAMA;
+    echo "\n";
+
+    echo "Club : ";
+    echo CLUB;
+    echo "\n";
+
+?>
---
+++ Pertemuan 3/TipeDataString.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo 'Name : ';
+    echo 'Elza Bhima';
+    echo "\n";
+
+    echo "Name : ";
+    echo "Elza\t Bhima\n";
+
+?>    
