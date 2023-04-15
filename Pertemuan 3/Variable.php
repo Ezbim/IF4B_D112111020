3bbf0b385de278c55577bedfb3d00613b093c6a6
ezbim <bhima.pilot@gmail.com>
2023-04-15 23:17:43 +0700

    Pertemuan 3

---
+++ Pertemuan 3/Variable.php
@@ -0,0 +1,24 @@
+<?php
+
+    $name       = "Elza Bhima";
+    $age        = 24;
+    $address    = "Batam";
+    $Elza      = "";
+
+    echo "Name : ";
+    echo $name;
+    echo "\n";
+    
+    echo "Age  : ";
+    echo $age;
+    echo "\n";
+
+    echo "Address  : ";
+    echo $address;
+    echo "\n";
+
+    echo "Elza : ";
+    echo $Elza;
+    echo "\n";
+
+?>
